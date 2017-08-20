eb050b756c76a8ba38325946c34e7ba19f3cdc05
Tu Luong <tluong@volute.co>
2017-08-19 22:06:21 -0400

    Update design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,17 @@
 jQuery(document).ready(function($) {
 	// Global variables
 	var speed = 1200,
-			autoplaySpeed = 4000;
+			autoplaySpeed = 4000,
+			viewportWidth = $(window).width(),
+			viewportHeight = $(window).height();;
 
 	// DOM elements
 	var $homeSlider = $('.home-slider'),
 			$toolSlider = $('.tool--infobox-slider'),
 			$toolsList = $('ul.tools'),
 			$toolSingle = $('li.tool'),
-			$toolInfoBox = $('.tool--infobox');
+			$toolInfoBox = $('.tool--infobox'),
+			$scrollDown = $('.scroll-down');
 
 	// Home Slider
 	$homeSlider.slick({
@@ -38,9 +41,7 @@ jQuery(document).ready(function($) {
 	// Tool effect on hover
 	$toolsList.hover(
 		function() {
-			if (!$(this).parents('.tools-wrapper').find('.tool--infobox').hasClass('tool--infobox__active')) {
-				$(this).addClass('tools__hover');
-			}
+			$(this).addClass('tools__hover');
 		},
 		function() {
 			$(this).removeClass('tools__hover');
@@ -50,12 +51,24 @@ jQuery(document).ready(function($) {
 	// Tool item on click
 	$toolSingle.bind('click', function() {
 		$(this).parents().eq(2).find('.tool--infobox').addClass('tool--infobox__active');
-		$(this).parents().find('ul.tools').removeClass('tools__hover');
+		$(this).removeClass('tool__not-selected').addClass('tool__selected');
+		$(this).siblings().removeClass('tool__selected').addClass('tool__not-selected');
+		if (viewportWidth >= 1024) {
+			$('html, body').animate({ scrollTop: $($(this).data('url')).offset().top }, 'slow');
+		}
 	});
 
+	// Close info box
 	$('.js-tool--infobox-close').bind('click', function(e) {
 		e.preventDefault();
 		$(this).parent().removeClass('tool--infobox__active');
+		$(this).parents().find('ul.tools').find('li.tool__selected').removeClass('tool__selected');
+		$(this).parents().find('ul.tools').find('li.tool__not-selected').removeClass('tool__not-selected');
 	});
 
+	// scroll down
+	$scrollDown.bind('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 'slow');
+	});
 });
