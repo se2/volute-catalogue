cfb70bffadc607c589103dc7c5ac04310659e3a6
Tu Luong <tluong@volute.co>
2017-08-20 16:16:38 -0400

    Populate real tools data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,19 +13,6 @@ jQuery(document).ready(function($) {
 			$toolInfoBox = $('.tool--infobox'),
 			$scrollDown = $('.scroll-down');
 
-	// Home Slider
-	$homeSlider.slick({
-		autoplay: true,
-		infinite: true,
-		slidesToShow: 1,
-		dots: true,
-		arrows: true,
-		speed: speed,
-		autoplaySpeed: autoplaySpeed,
-		fade: true,
-		pauseOnHover: true,
-	});
-
 	// Tool slider
 	$toolSlider.slick({
 		autoplay: true,
@@ -36,6 +23,7 @@ jQuery(document).ready(function($) {
 		speed: speed,
 		autoplaySpeed: autoplaySpeed,
 		fade: true,
+		mobileFirst: true
 	});
 
 	// Tool effect on hover
@@ -48,22 +36,67 @@ jQuery(document).ready(function($) {
 		}
 	);
 
+	var trimByWord = function(sentence, words) {
+	  var result = sentence;
+	  var resultArray = result.split(' ');
+	  if (resultArray.length > words) {
+	    resultArray = resultArray.slice(0, words);
+	    result = resultArray.join(' ') + '... <a href="#" class="tool--readmore"><strong>read more</strong></a>';
+	  }
+	  return result;
+	}
+
 	// Tool item on click
 	$toolSingle.bind('click', function() {
+		var url = $(this).data('url'),
+				id = $(this).data('id'),
+				$currentRow = $('#' + url);
+
 		$(this).parents().eq(2).find('.tool--infobox').addClass('tool--infobox__active');
 		$(this).removeClass('tool__not-selected').addClass('tool__selected');
 		$(this).siblings().removeClass('tool__selected').addClass('tool__not-selected');
-		if (viewportWidth >= 1024) {
-			$('html, body').animate({ scrollTop: $($(this).data('url')).offset().top }, 'slow');
+		if (viewportWidth > 1024) {
+			$('html, body').animate({ scrollTop: $('#' + url + ' .tool--infobox-slider-wrapper').offset().top - 90 }, 'slow');
+		} else if (viewportWidth = 1024) {
+			$('html, body').animate({ scrollTop: $('#' + url + ' .tool--infobox-slider-wrapper').offset().top - 120 }, 'slow');
 		}
+
+		// get data
+		$.getJSON('tools.json', function(data) {
+			var toolData = data[id - 1],
+					metadata = '';
+
+			$currentRow.find('.tool--infobox-metadata h1 strong').html(toolData.name);
+			$currentRow.find('.tool--infobox-metadata p.tool--description').html(toolData.description);
+
+			metadata +=
+						'<strong>Author:</strong> ' + toolData.author + ' <br>' +
+						'<strong>Developers:</strong> ' + toolData.developers.join(', ') + ' <br>' +
+						'<strong>Category:</strong> ' + toolData.category.join(', ') + ' <br>' +
+						'<strong>Release:</strong> ' + toolData.release + ' <br>' +
+						'<strong>Version:</strong> ' + toolData.version + ' <br>';
+
+			if (typeof toolData.related != 'undefined' && toolData.related.length > 0) {
+				metadata += '<strong>Related:</strong> ' + toolData.related.join(', ') + ' <br>';
+			}
+
+			metadata += '<strong>Responsive:</strong>&nbsp;' +
+						  			'<i class="fa fa-mobile" aria-hidden="true"></i>&nbsp;&nbsp;' +
+						  			'<i class="fa fa-tablet" aria-hidden="true"></i>&nbsp;&nbsp;' +
+						  			'<i class="fa fa-desktop" aria-hidden="true"></i> <br>';
+
+			$currentRow.find('.tool--infobox-metadata p.tool--metadata').html(metadata);
+		});
 	});
 
 	// Close info box
 	$('.js-tool--infobox-close').bind('click', function(e) {
+		var currentRow = '#' + $(this).data('row');
+
 		e.preventDefault();
 		$(this).parent().removeClass('tool--infobox__active');
-		$(this).parents().find('ul.tools').find('li.tool__selected').removeClass('tool__selected');
-		$(this).parents().find('ul.tools').find('li.tool__not-selected').removeClass('tool__not-selected');
+		$(currentRow).find('ul.tools').find('li.tool__selected').removeClass('tool__selected');
+		$(currentRow).find('ul.tools').find('li.tool__not-selected').removeClass('tool__not-selected');
 	});
 
 	// scroll down
