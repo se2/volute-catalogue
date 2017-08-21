dc3010acb473e673c1300960de346b14be7f999b
Tu Luong <tluong@volute.co>
2017-08-21 14:16:14 -0400

    Update Aug 21

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,7 @@ jQuery(document).ready(function($) {
 	// DOM elements
 	var $homeSlider = $('.home-slider'),
 			$toolSlider = $('.tool--infobox-slider'),
+			$toolSliderMobile = $('.tool--infobox-slider__mobile'),
 			$toolsList = $('ul.tools'),
 			$toolSingle = $('li.tool'),
 			$toolInfoBox = $('.tool--infobox'),
@@ -50,46 +51,73 @@ jQuery(document).ready(function($) {
 	$toolSingle.bind('click', function() {
 		var url = $(this).data('url'),
 				id = $(this).data('id'),
-				$currentRow = $('#' + url);
+				offsetHeight = 0,
+				$currentRow = $('#row-' + url);
 
 		if (viewportWidth > 768) {
+			$('li.tool').removeClass('tool__selected').removeClass('tool__not-selected');
+			$('.tool--infobox').removeClass('tool--infobox__active');
 			$(this).parents().eq(2).find('.tool--infobox').addClass('tool--infobox__active');
 			$(this).removeClass('tool__not-selected').addClass('tool__selected');
 			$(this).siblings().removeClass('tool__selected').addClass('tool__not-selected');
+		} else {
+			$('.tool--infobox__mobile').addClass('tool--infobox__mobile__active');
+
+			// Tool mobile slider
+			$toolSliderMobile.not('.slick-initialized').slick({
+				autoplay: true,
+				infinite: true,
+				slidesToShow: 1,
+				dots: false,
+				arrows: false,
+				speed: speed,
+				autoplaySpeed: autoplaySpeed,
+				fade: true,
+				mobileFirst: true
+			});
+		}
+
+		// recalcualte offset change
+		for (var i = 0; i < parseInt(url); i++) {
+			if (typeof $('#row-' + i + ' .tool--infobox-slider-wrapper').height() != 'undefined') {
+				offsetHeight += $('#row-' + i + ' .tool--infobox-slider-wrapper').height() - 1; // account for min-height: 1px;
+			}
 		}
 
 		// scroll to view
 		if (viewportWidth > 1024) {
-			$('html, body').animate({ scrollTop: $('#' + url + ' .tool--infobox-slider-wrapper').offset().top - 90 }, 'slow');
+			$('html, body').animate({ scrollTop: $('#row-' + url + ' .tool--infobox-slider-wrapper').offset().top - 90 - offsetHeight }, 1200);
 		} else if (viewportWidth > 768 && viewportWidth == 1024) {
-			$('html, body').animate({ scrollTop: $('#' + url + ' .tool--infobox-slider-wrapper').offset().top - 120 }, 'slow');
+			$('html, body').animate({ scrollTop: $('#row-' + url + ' .tool--infobox-slider-wrapper').offset().top - 120 - offsetHeight }, 1200);
 		}
 
 		// get data
 		$.getJSON('tools.json', function(data) {
 			var toolData = data[id - 1],
-					metadata = '';
-
-			$currentRow.find('.tool--infobox-metadata h1 strong').html(toolData.name);
-			$currentRow.find('.tool--infobox-metadata p.tool--description').html(toolData.description);
-
-			metadata +=
-						'<strong>Author:</strong> ' + toolData.author + ' <br>' +
-						'<strong>Developers:</strong> ' + toolData.developers.join(', ') + ' <br>' +
-						'<strong>Category:</strong> ' + toolData.category.join(', ') + ' <br>' +
-						'<strong>Release:</strong> ' + toolData.release + ' <br>' +
-						'<strong>Version:</strong> ' + toolData.version + ' <br>';
+					metadata =  '<strong>Author:</strong> ' + toolData.author + ' <br>' +
+											'<strong>Developers:</strong> ' + toolData.developers.join(', ') + ' <br>' +
+											'<strong>Category:</strong> ' + toolData.category.join(', ') + ' <br>' +
+											'<strong>Release:</strong> ' + toolData.release + ' <br>' +
+											'<strong>Version:</strong> ' + toolData.version + ' <br>';
 
 			if (typeof toolData.related != 'undefined' && toolData.related.length > 0) {
 				metadata += '<strong><i class="fa fa-puzzle-piece" aria-hidden="true"></i>:</strong> ' + toolData.related.join(', ') + ' <br>';
 			}
 
-			metadata += '<strong>Responsive:</strong>&nbsp;' +
-						  			'<i class="fa fa-mobile" aria-hidden="true"></i>&nbsp;&nbsp;' +
-						  			'<i class="fa fa-tablet" aria-hidden="true"></i>&nbsp;&nbsp;' +
-						  			'<i class="fa fa-desktop" aria-hidden="true"></i> <br>';
+			metadata += '<i class="fa fa-mobile" aria-hidden="true"></i>&nbsp;&nbsp;' +
+						  		'<i class="fa fa-tablet" aria-hidden="true"></i>&nbsp;&nbsp;' +
+						  		'<i class="fa fa-desktop" aria-hidden="true"></i> <br>';
 
-			$currentRow.find('.tool--infobox-metadata p.tool--metadata').html(metadata);
+			if (viewportWidth > 768) {
+				$currentRow.find('.tool--infobox-metadata h1 strong').html(toolData.name);
+				$currentRow.find('.tool--infobox-metadata p.tool--description').html(toolData.description);
+				$currentRow.find('.tool--infobox-metadata p.tool--metadata').html(metadata);
+			} else {
+				var $popupMobile = $('.tool--infobox__mobile');
+				$popupMobile.find('.tool--infobox-metadata h1 strong').html(toolData.name);
+				$popupMobile.find('.tool--infobox-metadata p.tool--description').html(toolData.description);
+				$popupMobile.find('.tool--infobox-metadata p.tool--metadata').html(metadata);
+			}
 		});
 	});
 
@@ -101,6 +129,9 @@ jQuery(document).ready(function($) {
 		$(this).parent().removeClass('tool--infobox__active');
 		$(currentRow).find('ul.tools').find('li.tool__selected').removeClass('tool__selected');
 		$(currentRow).find('ul.tools').find('li.tool__not-selected').removeClass('tool__not-selected');
+
+		// mobile
+		$('.tool--infobox__mobile').removeClass('tool--infobox__mobile__active');
 	});
 
 	// scroll down
